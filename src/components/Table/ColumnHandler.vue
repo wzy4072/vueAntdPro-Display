b2f464aa2deb90ee87f8c6aad275d4ad03dbc609
wangzeyang <wangzeyang@bytter.com>
2021-02-02 11:15:21 +0800

    拖拽顺滑版本

--- src/components/Table/ColumnHandler.vue
+++ src/components/Table/ColumnHandler.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="custom-columns-tool">
     <a-checkbox-group v-bind:value="checkedKeys">
-      <draggable class="list-group" tag="ul" :value="columns" v-bind="dragOptions" @end="onDragEnd">
+      <draggable class="list-group" tag="ul" v-model="dynamicColumns" v-bind="dragOptions" @end="onDragEnd">
         <transition-group type="transition" :name="'flip-list'">
           <li
             class="list-group-item col-tool-check-item"
-            v-for="(col, cIndex) in columns"
-            :key="cIndex + (col.dataIndex || col.title)"
+            v-for="(col, cIndex) in dynamicColumns"
+            :key="col.dataIndex || col.title"
           >
             <a-checkbox :value="col.dataIndex || col.title" @change="checkChange"> {{ col.title }} </a-checkbox>
             <a-icon type="vertical-align-top" @click="sortItem(cIndex, 0)" />
@@ -14,35 +14,13 @@
           </li>
         </transition-group>
       </draggable>
-      <div></div>
     </a-checkbox-group>
-
-    <!-- <a-row>
-      <a-row>
-        <a-checkbox
-          @change="colsChangeAll"
-          :indeterminate="checkedKeys.length > 0 && checkedKeys.length !== columns.length"
-          :checked="checkedKeys.length === columns.length"
-        >
-          列展示
-        </a-checkbox>
-        <a-button type="link" @click="restColumns"> 重置 </a-button>
-      </a-row>
-      <a-checkbox-group v-bind:value="checkedKeys">
-        <div v-for="(col, cIndex) in columns" :key="cIndex" class="col-tool-check-item">
-          <a-checkbox :value="col.dataIndex || col.title" @change="checkChange"> {{ col.title }} </a-checkbox>
-          <a-icon type="vertical-align-top" @click="sortItem(cIndex, 0)" />
-          <a-icon type="vertical-align-bottom" @click="sortItem(cIndex, columns.length)" />
-        </div>
-      </a-checkbox-group>
-    </a-row> -->
   </div>
 </template>
 
 <script>
 import draggable from 'vuedraggable'
 import cloneDeep from 'lodash.clonedeep'
-
 const message = ['vue.draggable', 'draggable', 'component', 'for', 'vue.js 2.0', 'based', 'on', 'Sortablejs']
 export default {
   components: {
@@ -52,40 +30,34 @@ export default {
 
   data() {
     return {
-      // dynamicColumns: [],
       list: message.map((name, index) => {
         return { name, order: index + 1, fixed: false }
       }),
       isDragging: false,
+      dynamicColumns: [],
     }
   },
   watch: {
-    // columns: {
-    //   handler(nVal) {
-    //     console.log('columns 改变了', nVal.map((item) => item.title).join('-'))
-    //     this.dynamicColumns = cloneDeep(nVal)
-    //   },
-    //   immediate: true,
-    // },
-    // dynamicColumns(nv) {
-    //   this.$emit('updatecolumns', nv)
-    // },
+    columns: {
+      handler(nVal) {
+        console.log('columns 改变了', nVal.map((item) => item.title).join('-'))
+        this.dynamicColumns = cloneDeep(nVal)
+      },
+      immediate: true,
+    },
   },
   computed: {
     dragOptions() {
       return {
         animation: 0,
-        // group: 'description',
-        // disabled: !this.editable,
         ghostClass: 'ghost',
       }
     },
   },
   methods: {
     onDragEnd({ to, from, item, clone, oldIndex, newIndex }) {
-      this.sortItem(oldIndex, newIndex)
+      this.$emit('updatecolumns', this.dynamicColumns)
     },
-    // },
     colsChangeAll(e) {
       const keys = e.target.checked ? this.columns.map((i) => i.dataIndex || i.title) : []
       this.$emit('updatecheckedKeys', keys)
